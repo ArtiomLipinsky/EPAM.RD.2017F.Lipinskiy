f640a1bd6f5fb030958a0ca368acde79c0e2c059
ArtiomLipinsky <artiomlipinsky@gmail.com>
2017-02-15 02:43:00 +0300

    Task1

---
+++ Day1/SampleService/MyServiceLibrary/StorageService.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace StorageServiceLibrary
+{
+
+    public class StorageService<T> where T : IUnique
+    {
+        #region Fields
+
+        private IIdGenerator idGenerator;
+        private IList<T> container;
+        private IEqualityComparer<T> comparer;
+        private IdComparer idComparer;
+
+        #endregion
+
+        #region Ctor
+
+        public StorageService()
+        {
+            idGenerator = GetDefaultIdGenerator();
+            container = new List<T>();
+            idComparer = new IdComparer();
+        }
+
+        public StorageService(IIdGenerator idGenerator, IList<T> container, IEqualityComparer<T> comparer) : base()
+        {
+            if (idGenerator == null) throw new ArgumentNullException(nameof(idGenerator));
+            if (container == null) throw new ArgumentNullException(nameof(container));
+            if (comparer == null) throw new ArgumentNullException(nameof(comparer));
+
+            this.idGenerator = idGenerator;
+            this.container = container;
+            this.comparer = comparer;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public void Add(T item)
+        {
+            if (item == null) throw new ArgumentNullException(nameof(item));
+
+            if (container.Contains(item, comparer)) throw new InvalidOperationException(nameof(item));
+
+            while (item.Id == 0 || container.Contains(item, idComparer)) item.Id = idGenerator.GetId();
+
+            container.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            if (item == null) throw new ArgumentNullException(nameof(item));
+            if (!container.Remove(item)) throw new InvalidOperationException(nameof(item));
+        }
+
+
+        public void Remove(int id)
+        {
+            if (id <= 0) throw new ArgumentOutOfRangeException(nameof(id));
+            if (!container.Remove(container.First(item => item.Id == id))) throw new InvalidOperationException();
+        }
+
+        public T Get(int id)
+        {
+            if (id <= 0) throw new ArgumentOutOfRangeException(nameof(id));
+            return container.First(item => item.Id == id);
+        }
+
+
+        public IEnumerable<T> Search(Func<T, bool> predicate)
+        {
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+            return container.Where(predicate).AsEnumerable();
+        }
+
+
+        public IEnumerable<T> AsEnumerable()
+        {
+            return container.AsEnumerable();
+        }
+
+        public IList<T> ToList()
+        {
+            return container.ToList();
+        }
+
+        public T[] ToArray()
+        {
+            return container.ToArray();
+        }
+
+
+
+        static public IList<T> GetDefaultContainer()
+        {
+            return new List<T>();
+        }
+
+        static public IIdGenerator GetDefaultIdGenerator()
+        {
+            return new IncrementIdGenerator();
+        }
+
+        static public IEqualityComparer<T> GetDefaultComparer()
+        {
+            return new IdComparer();
+        }
+
+        #endregion
+
+        #region Nested clases
+
+        private class IncrementIdGenerator : IIdGenerator
+        {
+            int counter = 0;
+
+            public int GetId()
+            {
+                return counter++;
+            }
+        }
+
+        private class IdComparer : IEqualityComparer<T>
+        {
+            public bool Equals(T x, T y)
+            {
+                if (x.Id == y.Id) return true;
+                return false;
+            }
+
+            public int GetHashCode(T obj)
+            {
+                return obj.Id; //!!!
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ Day1/SampleService/ServiceApplication/Program.cs
@@ -0,0 +1,46 @@
+﻿using StorageServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ServiceApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try {
+
+            var userStorageService = new StorageService<User>();
+
+            // 1. Add a new user to the storage.
+
+            User user_1 = new User() { FirstName = "Donald", LastName = "Tramp", DateOfBirth = new DateTime(1946, 6, 14) };
+            User user_2 = new User() { FirstName = "TestUserFirstName1", LastName = "TestUserLastName1", DateOfBirth = new DateTime(2000, 1, 1) };
+            User user_3 = new User() { FirstName = "TestUserFirstName2", LastName = "TestUserLastName2", DateOfBirth = new DateTime(2000, 1, 1) };
+            userStorageService.Add(user_1);
+            userStorageService.Add(user_2);
+
+            // 2. Remove an user from the storage.
+
+            user_1 = userStorageService.AsEnumerable().First();
+            userStorageService.Remove(user_1);
+
+            // 3. Search for an user by the first name.
+
+            IEnumerable<User> searchResult_1 = userStorageService.Search(u => u.FirstName.Contains("TestUserFirstName"));
+
+            // 4. Search for an user by the last name.
+
+            IEnumerable<User> searchResult_2 = userStorageService.Search(u => u.LastName.Contains("TestUserLastName"));
+
+            Console.ReadKey();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
