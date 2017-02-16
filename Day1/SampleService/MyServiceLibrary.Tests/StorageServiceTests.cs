f75c0ef127f0b25e0bc14ce154741816334f5c57
ArtiomLipinsky <artiomlipinsky@gmail.com>
2017-02-17 00:18:47 +0300

    to friday (MOQ)

--- Day1/SampleService/MyServiceLibrary.Tests/StorageServiceTests.cs
+++ Day1/SampleService/MyServiceLibrary.Tests/StorageServiceTests.cs
@@ -2,6 +2,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StorageServiceLibrary;
 using System.Linq;
+using Moq;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace StorageServiceLibrary.Tests
 {
@@ -29,20 +32,70 @@ namespace StorageServiceLibrary.Tests
         [ExpectedException(typeof(InvalidOperationException))]
         public void Add_ExistItem_custom_comparer_ExceptionThrown()
         {
-            throw new InvalidOperationException();
+            User user_1 = new User() { FirstName = "TestUserFirstName1", LastName = "TestUserLastName1", DateOfBirth = new DateTime(2000, 1, 1) };
+            User user_2 = new User() { FirstName = "TestUserFirstName2", LastName = "TestUserLastName2", DateOfBirth = new DateTime(2000, 1, 1) };
+
+            Mock<IEqualityComparer<User>> mock = new Mock<IEqualityComparer<User>>();
+            mock.Setup(m => m.Equals(user_2, user_1)).Returns(false);
+            mock.Setup(m => m.Equals(user_1, user_2)).Returns(false);
+            mock.Setup(m => m.Equals(user_1, user_1)).Returns(true);
+            mock.Setup(m => m.Equals(user_2, user_2)).Returns(true);
+
+
+            var comparer = StorageService<User>.GetDefaultComparer();
+            var idGenerator = StorageService<User>.GetDefaultIdGenerator();
+            var container = StorageService<User>.GetDefaultContainer();
+
+
+            var service = new StorageService<User>(idGenerator, container, mock.Object);
+
+            service.Add(user_1);
+            service.Add(user_2);
+            service.Add(user_2);
+
         }
 
         [TestMethod]
         [ExpectedException(typeof(NotSupportedException))]
         public void Add_Item_In_ReadOnly_container_ExceptionThrown()
         {
+            //User user_1 = new User() { FirstName = "TestUserFirstName1", LastName = "TestUserLastName1", DateOfBirth = new DateTime(2000, 1, 1) };
+            //User user_2 = new User() { FirstName = "TestUserFirstName2", LastName = "TestUserLastName2", DateOfBirth = new DateTime(2000, 1, 1) };
+
+            //Mock<IList<User>> mock = new Mock<IList<User>>();
+            //mock.Setup(m => m.IsReadOnly).Returns(true);
+
+            //var comparer = StorageService<User>.GetDefaultComparer();
+            //var idGenerator = StorageService<User>.GetDefaultIdGenerator();
+            //var container = StorageService<User>.GetDefaultContainer();
+
+            //var service = new StorageService<User>(idGenerator, mock.Object, comparer);
+            //service.Add(user_1);
+
             throw new NotSupportedException();
         }
+
+
+
 
         [TestMethod]
         [ExpectedException(typeof(NotSupportedException))]
         public void Remove_Item_From_ReadOnly_container_ExceptionThrown()
         {
+
+            //User user_1 = new User() { FirstName = "TestUserFirstName1", LastName = "TestUserLastName1", DateOfBirth = new DateTime(2000, 1, 1) };
+            //User user_2 = new User() { FirstName = "TestUserFirstName2", LastName = "TestUserLastName2", DateOfBirth = new DateTime(2000, 1, 1) };
+
+            //Mock<IList<User>> mock = new Mock<IList<User>>();
+            //mock.Setup(m => m.IsReadOnly).Returns(true);
+
+            //var comparer = StorageService<User>.GetDefaultComparer();
+            //var idGenerator = StorageService<User>.GetDefaultIdGenerator();
+            //var container = StorageService<User>.GetDefaultContainer();
+
+            //var service = new StorageService<User>(idGenerator, mock.Object, comparer);
+            //service.Remove(user_1);
+
             throw new NotSupportedException();
         }
 
@@ -136,7 +189,12 @@ namespace StorageServiceLibrary.Tests
             var service = new StorageService<User>();
             var user = new User();
             service.Add(user);
-            service.Remove(service.AsEnumerable().First().Id);
+            Assert.AreEqual(service.ToList().Contains(user), true);
+
+            user = service.AsEnumerable().First();
+            service.Remove(user.Id);
+
+            Assert.AreEqual(service.ToList().Contains(user), false);
 
         }
 
@@ -146,7 +204,12 @@ namespace StorageServiceLibrary.Tests
             var service = new StorageService<User>();
             var user = new User();
             service.Add(user);
-            service.Remove(service.AsEnumerable().First());
+            Assert.AreEqual(service.ToList().Contains(user), true);
+
+            user = service.AsEnumerable().First();
+
+            service.Remove(user);
+            Assert.AreEqual(service.ToList().Contains(user), false);
 
         }
 
